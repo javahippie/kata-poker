635940d87b92832ec15db8575e19202910fd5953
Tim Zöller <mail@tim-zoeller.de>
2015-09-10 16:06:42 +0200

    Finished implementation for "by-high-card"

--- src/kata_poker/hand_comparator.clj
+++ src/kata_poker/hand_comparator.clj
@@ -3,16 +3,19 @@
   (:require [kata-poker.card-comparator :as cards]))
 
 (defn- by-high-card
+  "Compares both hands regarding by who has got the higher card"
   [black white]
-  (let [values-black (cards/hand-to-values black)
-        values-white (cards/hand-to-values white)]
-    '(values-black values-white)))
+  (let [tuples (map vector (sort > (cards/hand-to-values black)) (sort > (cards/hand-to-values white)))
+        first-non-matching (take 1 (drop-while #(= (first %) (second %)) tuples))]
+     (compare (first first-non-matching)
+              (second first-non-matching))))
 
 
 (defn compare-hands
   [black white])
 
 
-(by-high-card '("2H" "3D" "5S" "9C" "KD")
-               '("2C" "3H" "4S" "8C" "AH"))
+(by-high-card '("2H" "3D" "4S" "8C" "AD")
+               '("2C" "3H" "4S" "7C" "AH"))
+
 
